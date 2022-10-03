75a3c2568aaf5e76b095c99f512c782e38cb1763
lauro <lauropimentel12@gmail.com>
2022-10-03 14:35:26 -0300

    f

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -84,13 +84,15 @@ WSGI_APPLICATION = "django_portfolio.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASES = { 
+      'default': dj_database_url.config(default=env('DATABASE_URL'))
+        
     }
-}
-
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'dbtest.sqlite3'
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
