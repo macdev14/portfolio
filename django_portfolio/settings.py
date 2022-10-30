07ffc8d8c7876ae6064cfb985ed1acdff268c7bf
lauro <lauropimentel12@gmail.com>
2022-10-29 22:47:09 -0300

    www

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lauromtp.herokuapp.com', 'localhost', 'lauromtp.com' ]
+ALLOWED_HOSTS = ['lauromtp.herokuapp.com', 'localhost', 'lauromtp.com', 'www.lauromtp.com' ]
 
 
 # Application definition
